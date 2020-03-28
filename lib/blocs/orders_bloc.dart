41c58107a86e3601b35b69489c1969ab80f53807
origin <https://github.com/douglasnatanmartins/Admin_loja.git>
2020-03-27 22:32:32 -0300

    Star Commit

--- lib/blocs/orders_bloc.dart
+++ lib/blocs/orders_bloc.dart
@@ -1,6 +1,8 @@
- import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:rxdart/rxdart.dart';
+
+enum SortCriteria {READY_FIRST, READY_LAST, NONE}
 
 class OrdersBloc extends BlocBase {
 
@@ -8,57 +10,91 @@ class OrdersBloc extends BlocBase {
 
   Stream<List> get outOrders => _ordersController.stream;
 
+  Firestore _firestore = Firestore.instance;
+
   List<DocumentSnapshot> _orders = [];
 
-  Firestore _firestore = Firestore.instance;
+  SortCriteria _criteria;
 
   OrdersBloc(){
     _addOrdersListener();
   }
+
   void _addOrdersListener(){
+    _orders.sort((a, b) => a.documentID.compareTo(b.documentID));
     _firestore.collection("orders").snapshots().listen((snapshot){
       snapshot.documentChanges.forEach((change){
-          String oid = change.document.documentID;
-          _orders.sort((a,b) => a.documentID.compareTo(b.documentID));
+        String oid = change.document.documentID;
 
-          switch(change.type){
-            case DocumentChangeType.added:
-              _orders.add(change.document);
-              break;
-            case DocumentChangeType.modified:
-             // int i = 0;
-             // int index = 0;
-              _orders.removeWhere((order) => order.documentID == oid);
-              _orders.add(change.document);
-              //_orders.removeWhere((order){
-               // if(order.documentID == oid){
-                 // index = 1;
-                  //return true;
-                //} else {
-                 // ++i;
-                  //return false;
-               // }
-             // });
-             // _orders.insert(index, change.document);
-
-              break;
-            case DocumentChangeType.removed:
-              _orders.retainWhere((order) => order.documentID == oid);
-              break;
-          }
+        switch(change.type){
+          case DocumentChangeType.added:
+            _orders.add(change.document);
+            break;
+          case DocumentChangeType.modified:
+            int i = 0;
+            int index = 0;
+           // _orders.removeWhere((order) => order.documentID == oid);
+           // _orders.add(change.document);
+            _orders.removeWhere((order){
+              if (order.documentID == oid){
+                index = i;
+                return true;
+              } else {
+                ++i;
+                return false;
+              }
+            });
+            _orders.insert(index, change.document);
+            _criteria = SortCriteria.NONE;
+            break;
+          case DocumentChangeType.removed:
+            _orders.removeWhere((order) => order.documentID == oid);
+            break;
+        }
       });
 
-      _ordersController.add(_orders);
+      _sort();
     });
+  }
 
+  void setOrderCriteria(SortCriteria criteria){
+    _criteria = criteria;
+
+    _sort();
+  }
+
+  void _sort(){
+    switch(_criteria){
+      case SortCriteria.READY_FIRST:
+        _orders.sort((a,b){
+          int sa = a.data["status"];
+          int sb = b.data["status"];
+
+          if(sa < sb) return 1;
+          else if(sa > sb) return -1;
+          else return 0;
+        });
+        break;
+      case SortCriteria.READY_LAST:
+        _orders.sort((a,b){
+          int sa = a.data["status"];
+          int sb = b.data["status"];
+
+          if(sa > sb) return 1;
+          else if(sa < sb) return -1;
+          else return 0;
+        });
+        break;
+      case SortCriteria.NONE:
+        break;
+    }
+
+    _ordersController.add(_orders);
   }
 
   @override
   void dispose() {
-
     _ordersController.close();
-
   }
-
 
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:gerenciadorloja/blocs/orders_bloc.dart';
 import 'package:gerenciadorloja/blocs/user_bloc.dart';
 import 'package:gerenciadorloja/tabs/orders_tab.dart';
@@ -100,6 +101,44 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
+      floatingActionButton: _buildFloating(),
     );
   }
+  Widget _buildFloating(){
+    switch(_page){
+      case 0:
+        return null;
+      case 1:
+        return SpeedDial(
+          animationSpeed: 1,
+          child: Icon(Icons.sort),
+          backgroundColor: Colors.purple,
+          overlayOpacity: 0.4,
+          overlayColor: Colors.black,
+          children: [
+            SpeedDialChild(
+              child: Icon(Icons.arrow_downward, color: Colors.purple),
+              backgroundColor: Colors.white,
+              label: "Concluidos Abaixo",
+              labelStyle: TextStyle(fontSize: 14),
+              onTap: (){
+                _ordersBloc.setOrderCriteria(SortCriteria.READY_LAST);
+              }
+            ),
+            SpeedDialChild(
+                child: Icon(Icons.arrow_upward, color: Colors.purple),
+                backgroundColor: Colors.white,
+                label: "Concluidos Acima",
+                labelStyle: TextStyle(fontSize: 14),
+                onTap: (){
+                    _ordersBloc.setOrderCriteria(SortCriteria.READY_FIRST);
+
+                }
+            ),
+          ]
+          ,
+        );
+    }
+  }
+
 }
